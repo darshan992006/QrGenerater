140940641990da8deb9c739349605b56acd837fe
Darshan Pathak <darshanpathak8899@gmail.com>
2024-04-10 13:26:51 +0530

    update

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ let qrImg =document.getElementById('qrImg');
 let qrText=document.getElementById('qrText');
 
 function generateQR(){
-    if (qrText.value.length>0) {
+    if (qrText.value.length>0){
         qrImg.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+ qrText.value;
-        imgBox.classList.add("showImg")
+        imgBox.classList.add("showImg");
     }
     else{
         qrText.classList.add("error");
